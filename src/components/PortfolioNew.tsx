02911224215e2c8a7e205d188999f14324273969
kushagrmittal1 <kushagr.mittal1@gmail.com>
2025-11-06 17:01:03 +0530

    update

--- src/components/PortfolioNew.tsx
+++ src/components/PortfolioNew.tsx
@@ -66,7 +66,7 @@ export const PortfolioNew = () => {
         </AnimatedSplitText>
       </div>
       {loading ? (
-        <div className="text-center text-white">Loading projects...</div>
+        <div className="text-center text-white">Loading projects....</div>
       ) : (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-3 lg:gap-10">
           {projects.map((project) => (
